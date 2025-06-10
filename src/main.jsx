90d276a401de326cfc706633de3b0c07999ba606
Parv3008 <parvgosani30@gmail.com>
2025-06-10 11:42:29 +0530

    Fix issues

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Root from './Root';
+import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
   <Provider store={store}>
     <Root/> 
   </Provider>
+  </StrictMode>
 );
