2e1db213507cc6be9986b82042e5c06c8fb6df12
Parv3008 <parvgosani30@gmail.com>
2025-06-10 11:40:30 +0530

    Fix issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -11,4 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Root/> 
   </Provider>
 );
-
